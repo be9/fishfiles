59761ec9d83b4b6974182950914a81ef13f864b3
Oleg Dashevskii <olegdashevskii@gmail.com>
2018-11-02 13:11:47 +0700

    fisherman 3

--- config.fish
+++ config.fish
@@ -3,3 +3,8 @@ set -gx AWS_PROFILE saml
 
 set -gx NVM_DIR "$HOME/.nvm"
 set -gx nvm_prefix /usr/local/opt/nvm
+
+set -g fisher_path "$HOME/.config/fish/fisherman"
+
+set fish_function_path $fish_function_path $fisher_path/functions
+set fish_complete_path $fish_complete_path $fisher_path/completions
