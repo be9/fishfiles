3894d8e280964b82039fdcbd281b37fa3e6f2534
oleg dashevskii <olegdashevskii@gmail.com>
2020-02-29 10:36:10 +0700

    Fix

--- config.fish
+++ config.fish
@@ -3,5 +3,9 @@ set -gx nvm_prefix /usr/local/opt/nvm
 
 set -g fisher_path "$HOME/.config/fish/fisherman"
 
-set fish_function_path $fish_function_path $fisher_path/functions
-set fish_complete_path $fish_complete_path $fisher_path/completions
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
